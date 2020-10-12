e3bd3741e3b0c266cd876bab3cea47e549509b22
asad149 <asad.arshad.94849@gmail.com>
2020-10-13 03:18:45 +0500

    Successfully Posting Data Programtically into Firebase

--- src/Components/Feed/MessageSender/MessageSender.js
+++ src/Components/Feed/MessageSender/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from "@material-ui/icons/Videocam";
 import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import { useStateValue } from "../../../StateMangement/StateProvider";
+import db from "../../../firebase";
+import firebase from "firebase";
 
 const MessageSender = () => {
   const [{ user }, dispatch] = useStateValue();
@@ -14,7 +16,13 @@ const MessageSender = () => {
     e.preventDefault();
 
     // some clever DB stuff
-
+    db.collection("posts").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      profilePic: user.photoURL,
+      username: user.displayName,
+      image: imageUrl,
+    });
     //Reset Input
     setInput("");
     setImageUrl("");
