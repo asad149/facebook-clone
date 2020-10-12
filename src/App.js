f9f1ac8b74951d560b4b163b067d0707d7627040
asad149 <asad.arshad.94849@gmail.com>
2020-10-13 01:12:13 +0500

    Configured Firebase and Done Firebase Google Authentication

--- src/App.js
+++ src/App.js
@@ -5,18 +5,27 @@ import Sidebar from "././Components/Sidebar/Sidebar";
 import Feed from "././Components/Feed/Feed";
 import Widgets from "././Components/Widgets/Widgets";
 
+import Login from "././Components/Login/Login";
+
 import "./App.css";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyCGUyP02hYRyy1HUlnKyxdixr62JbnD6uU",
   authDomain: "facebook-clone-58d55.firebaseapp.com",
@@ -9,3 +9,14 @@ const firebaseConfig = {
   appId: "1:426362781193:web:7deae2ec113000ef4c42d8",
   measurementId: "G-3DHD1E3632",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+
+export default db;
